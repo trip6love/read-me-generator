bf31d34948db0e5258e662ba31f46096e012e617
trip6love <lanebyers13@gmail.com>
2021-12-10 17:48:11 -0500

    added the array of questions

--- index.js
+++ index.js
@@ -4,13 +4,163 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter your project title!',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please proive a title!');
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        name: 'repository',
+        message:'Please enter the title of your repository!',
+        validate: repoInput => {
+            if (repoInput) {
+                return true;
+            } else {
+                console.log('Please enter a repository!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message:'Please enter your Github username!',
+        validate: hubInput => {
+            if (hubInput) {
+                return true;
+            } else {
+                console.log('Please enter a username!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'disc',
+        message:'Please enter a discription of your work!',
+        validate: discInput => {
+            if (discInput) {
+                return true;
+            } else {
+                console.log('Please enter a discription!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'use',
+        message:'Please enter details on using your finished product!',
+        validate: useInput => {
+            if (useInput) {
+                return true;
+            } else {
+                console.log('Please provide details on usage!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contact',
+        message:'Please enter your email for any further questions!',
+        validate: contactInput => {
+            if (contactInput) {
+                return true;
+            } else {
+                console.log('Please enter a email!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message:'Please enter testing instructions for your application!',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('Please enter test instructions!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message:'Please enter any packages needed for this application!',
+        validate: installInput => {
+            if (installInput) {
+                return true;
+            } else {
+                console.log('Please Enter installation information!')
+                return false;
+                
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message:'Please enter guidelines for anyone wanting to contribute!',
+        validate: contriInput => {
+            if (contriInput) {
+                return true;
+            } else {
+                console.log('Please enter any guidelines the user needs to know about!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message:'Please enter licensing information!',
+        choices: ['GNU', 'MIT', 'Apache 2.0', 'ISC'],
+        validate: licenseInput => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log('Please select')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'toc',
+        message:'Please enter a table of contents!',
+        validate: tocInput => {
+            if (tocInput) {
+                return true;
+            } else {
+                console.log('Please enter a table of contents!')
+                return false;
+            }
+        }
+    },
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquierer.prompt(questions);
+};
 
 // Function call to initialize app
 init();
