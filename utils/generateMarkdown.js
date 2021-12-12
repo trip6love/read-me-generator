25d95889e4c9b9778a858dd3ce00b11929352b5f
trip6love <lanebyers13@gmail.com>
2021-12-12 00:33:37 -0500

    Adding the License badge and license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license) {
+    return `![${license} License](https://img.shields.io/badge/license-${license.split(' ').join('%20')}-green)`;
+  } else {
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license) {
+    return `This is licensed under ${license} license.`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
